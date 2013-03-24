9200b08785a1b0612aa5f70c9a95a28a7f61b788
Gavin M. Roy <gmr@meetme.com>
2013-03-24 01:15:31 -0400

    Improve setup.py for future releases

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup
+import sys
+
+tests_require = ['mock', 'pylint', 'pep8']
+if sys.version_info < (2, 7, 0):
+    tests_require.append('unittest2')
+
+desc = 'A minimalistic & pythonic AMQP library focused on supporting RabbitMQ'
 
 setup(name='rmqid',
       version='0.1.0',
-      description='Minimalistic and Python RabbitMQ Focused AMQP library',
+      description=desc,
       author='Gavin M. Roy',
       author_email='gavinmroy@gmail.com',
       url='http://github.com/gmr/rmqid',
       packages=['rmqid'],
       install_requires=['pamqp'],
-      tests_require=['mock', 'unittest2', 'pylint', 'pep8'],
-      test_suite = "nose.collector",
+      tests_require=tests_require,
+      test_suite='nose.collector',
       license='BSD',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Topic :: Communications',
-          'Topic :: Internet',
-          'Topic :: Software Development :: Libraries',
-          ],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Topic :: Communications',
+                   'Topic :: Internet',
+                   'Topic :: Software Development :: Libraries'],
       zip_safe=True)
