4223dd98c41da0fa1662f2bd12bcba278b34d308
Gavin M. Roy <gmr@meetme.com>
2013-08-10 19:06:18 -0400

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
 from setuptools import setup
 import sys
+from rabbitpy import __version__
 
-tests_require = ['mock', 'pylint', 'pep8']
+tests_require = ['nose', 'mock']
 if sys.version_info < (2, 7, 0):
     tests_require.append('unittest2')
 
 desc = 'A minimalistic & pythonic AMQP library focused on supporting RabbitMQ'
 
 setup(name='rabbitpy',
-      version='0.5.0p1',
+      version=__version__,
       description=desc,
+      long_description=open('README.md').read(),
       author='Gavin M. Roy',
       author_email='gavinmroy@gmail.com',
-      url='http://github.com/gmr/rabbitpy',
+      url='http://rabbitpy.readthedocs.org',
       packages=['rabbitpy'],
+      package_data={'': ['LICENSE', 'README.md']},
+      include_package_data=True,
       install_requires=['pamqp>=1.2.0', 'requests>=1.0.0'],
       tests_require=tests_require,
       test_suite='nose.collector',
-      license='BSD',
+      license=open('LICENSE').read(),
       classifiers=['Development Status :: 3 - Alpha',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: BSD License',
                    'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 2.6',
                    'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.3',
                    'Topic :: Communications',
                    'Topic :: Internet',
