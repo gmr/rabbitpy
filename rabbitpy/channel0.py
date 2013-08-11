7b9a3c43672c499a37de35b13e2d720754d48e75
Gavin M. Roy <gmr@meetme.com>
2013-08-10 20:24:44 -0400

    Multiple fixes for Channel0
    - Python3 queue instead of Queue
    - Uses rabbitpy.events
    - Don't close if already closing or closed
    - Cleanly exit once Connection.Close frame is sent
    - Add maximum_channels property
    - Clean up connection flow
    - Refactor most methods in one way or another

--- rabbitpy/channel0.py
+++ rabbitpy/channel0.py
@@ -5,16 +5,19 @@ client on channel 0.
 """
 import locale
 import logging
-import Queue as queue
+try:
+    import queue
+except ImportError:
+    import Queue as queue
 import sys
 import threading
 
-from pamqp import exceptions
 from pamqp import header
 from pamqp import specification
 
 from rabbitpy import __version__
 from rabbitpy import base
+from rabbitpy import events
 
 LOGGER = logging.getLogger(__name__)
 
@@ -36,8 +39,7 @@ class Channel0(threading.Thread, base.AMQPChannel):
         super(Channel0, self).__init__(group, target, name, args, kwargs)
         self._channel_number = 0
         self._args = kwargs['connection_args']
-        self._close_event = kwargs['close']
-        self._on_connected = kwargs['connected']
+        self._events = kwargs['events']
         self._read_queue = kwargs['inbound']
         self._write_queue = kwargs['outbound']
         self._heartbeat = 0
@@ -47,25 +49,42 @@ class Channel0(threading.Thread, base.AMQPChannel):
         self.minimum_frame_size = specification.FRAME_MIN_SIZE
 
     def close(self):
-        self._write_frame(self._build_close_frame())
-        self._set_state(self.CLOSED)
+        if self.open:
+            self._set_state(self.CLOSING)
+            self._write_frame(self._build_close_frame())
+            self._events.set(events.CHANNEL0_CLOSED)
+            self._set_state(self.CLOSED)
+        else:
+            LOGGER.info('Bypassing sending Connection.Close, already closed')
+
+    @property
+    def maximum_channels(self):
+        return self._maximum_channels
 
     def run(self):
         self._set_state(self.OPENING)
         self._write_protocol_header()
-        if not self._connection_start():
-            return
-        self._connection_tune()
-        self._connection_open()
-        self._on_connected.set()
-        self._set_state(self.OPEN)
 
-        while self.open and not self._close_event.is_set():
-            try:
-                frame_value = self._read_queue.get(True, 0.5)
-                LOGGER.debug('Received frame: %r', frame_value)
-            except queue.Empty:
-                pass
+        # If we can negotiate a connection, do so
+        if self._on_connection_start(self._wait_on_frame()):
+            self._write_frame(self._build_start_ok_frame())
+            self._on_connection_tune(self._wait_on_frame())
+            self._write_frame(self._build_open_frame())
+            self._on_connection_openok(self._wait_on_frame())
+
+            # Now fully negotiated, notify connection and set state
+            self._set_state(self.OPEN)
+            self._events.set(events.CHANNEL0_OPENED)
+
+            # Loop as long as the connection is open, waiting for RPC requests
+            while self.open and not self._events.is_set(events.CHANNEL0_CLOSE):
+                try:
+                    frame_value = self._read_queue.get(True, 0.5)
+                    LOGGER.debug('Received frame: %r', frame_value)
+                except queue.Empty:
+                    pass
+
+        # Close out Channel0
         self.close()
 
     def _build_close_frame(self):
@@ -116,59 +135,6 @@ class Channel0(threading.Thread, base.AMQPChannel):
                                                self.maximum_frame_size,
                                                self._heartbeat)
 
-    def _connection_close(self):
-        self._write_frame(self._build_close_frame())
-        self._validate_frame(self._wait_on_frame(),
-                             specification.Connection.CloseOk)
-
-    def _connection_open(self):
-        """Negotiate the Connection.Start process, writing out a
-        Connection.StartOk frame when the Connection.Start frame is received.
-
-        """
-        self._write_frame(self._build_open_frame())
-        frame_value = self._wait_on_frame()
-        self._validate_frame(frame_value, specification.Connection.OpenOk)
-        self._on_connected.set()
-
-    def _connection_start(self):
-        """Negotiate the Connection.Start process, writing out a
-        Connection.StartOk frame when the Connection.Start frame is received.
-
-        :rtype: bool
-
-        """
-        frame_value = self._wait_on_frame()
-        self._validate_frame(frame_value, specification.Connection.Start)
-        if not self._validate_connection_start(frame_value):
-            LOGGER.error('Could not negotiate a connection, disconnecting')
-            return False
-        self._properties = frame_value.server_properties
-        for key in self._properties:
-            if key == 'capabilities':
-                for capability in self._properties[key]:
-                    LOGGER.debug('Server supports %s: %r',
-                                 capability, self._properties[key][capability])
-            else:
-                LOGGER.debug('Server %s: %r', key, self._properties[key])
-        self._write_frame(self._build_start_ok_frame())
-        return True
-
-    def _connection_tune(self):
-        """Negotiate the Connection.Tune frames, waiting for the Connection.Tune
-        frame from RabbitMQ and sending the Connection.TuneOk frame.
-
-        """
-        frame_value = self._wait_on_frame()
-        self._validate_frame(frame_value, specification.Connection.Tune)
-
-        self._maximum_channels = frame_value.channel_max
-        if frame_value.frame_max != self.maximum_frame_size:
-            self.maximum_frame_size = frame_value.frame_max
-        if frame_value.heartbeat:
-            self._heartbeat = frame_value.heartbeat
-        self._write_frame(self._build_tune_ok_frame())
-
     @property
     def _credentials(self):
         """Return the marshaled credentials for the AMQP connection.
@@ -189,6 +155,51 @@ class Channel0(threading.Thread, base.AMQPChannel):
             return locale.getdefaultlocale()[0]
         return self._args['locale']
 
+
+    def _on_connection_openok(self, frame_value):
+        """Negotiate the Connection.Start process, writing out a
+        Connection.StartOk frame when the Connection.Start frame is received.
+
+        :param frame_value pamqp.specification.Connection.OpenOk: OpenOk frame
+
+        """
+        self._validate_frame(frame_value, specification.Connection.OpenOk)
+
+    def _on_connection_start(self, frame_value):
+        """Negotiate the Connection.Start process, writing out a
+        Connection.StartOk frame when the Connection.Start frame is received.
+
+        :param frame_value pamqp.specification.Connection.Start: Start frame
+        :rtype: bool
+
+        """
+        if not self._validate_connection_start(frame_value):
+            LOGGER.error('Could not negotiate a connection, disconnecting')
+            return False
+        self._properties = frame_value.server_properties
+        for key in self._properties:
+            if key == 'capabilities':
+                for capability in self._properties[key]:
+                    LOGGER.debug('Server supports %s: %r',
+                                 capability, self._properties[key][capability])
+            else:
+                LOGGER.debug('Server %s: %r', key, self._properties[key])
+        return True
+
+    def _on_connection_tune(self, frame_value):
+        """Negotiate the Connection.Tune frames, waiting for the Connection.Tune
+        frame from RabbitMQ and sending the Connection.TuneOk frame.
+
+        :param frame_value pamqp.specification.Connection.Tune: Tuning frame
+
+        """
+        self._maximum_channels = frame_value.channel_max
+        if frame_value.frame_max != self.maximum_frame_size:
+            self.maximum_frame_size = frame_value.frame_max
+        if frame_value.heartbeat:
+            self._heartbeat = frame_value.heartbeat
+        self._write_frame(self._build_tune_ok_frame())
+
     def _process_server_rpc(self, value):
         """Process a RPC frame received from the server
 
@@ -197,7 +208,6 @@ class Channel0(threading.Thread, base.AMQPChannel):
         :raises: rabbitpy.exceptions.ChannelClosedException
         :raises: rabbitpy.exceptions.ConnectionClosedException
 
-        """
         if value.name == 'Channel.Close':
             LOGGER.warning('Received remote close for channel %i', channel_id)
             self._channels[channel_id]._remote_close()
@@ -218,7 +228,8 @@ class Channel0(threading.Thread, base.AMQPChannel):
             LOGGER.warning('Connection is no longer blocked')
             self._blocked = False
         else:
-            LOGGER.critical('Unhandled RPC request: %r', value)
+        """
+        LOGGER.critical('Unhandled RPC request: %r', value)
 
     def _validate_connection_start(self, frame_value):
         """Validate the received Connection.Start frame
@@ -239,5 +250,3 @@ class Channel0(threading.Thread, base.AMQPChannel):
     def _write_protocol_header(self):
         """Send the protocol header to the connected server."""
         self._write_frame(header.ProtocolHeader())
-
-
