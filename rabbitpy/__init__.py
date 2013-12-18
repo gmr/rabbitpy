6d7c1172ff156f376c61476bccf9912598059d19
Gavin M. Roy <gavinmroy@gmail.com>
2013-12-18 18:11:53 -0500

    Fix order of operations issues

--- rabbitpy/__init__.py
+++ rabbitpy/__init__.py
@@ -1,6 +1,20 @@
 __version__ = '0.10.0'
 version = __version__
 
+DEBUG = False
+
+import logging
+
+try:
+    from logging import NullHandler
+except ImportError:
+    # Python 2.6 does not have a NullHandler
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger('rabbitpy').addHandler(NullHandler())
+
 from rabbitpy.connection import Connection
 from rabbitpy.exchange import Exchange
 from rabbitpy.message import Message
@@ -16,17 +30,3 @@ from rabbitpy.simple import create_direct_exchange
 from rabbitpy.simple import create_fanout_exchange
 from rabbitpy.simple import create_topic_exchange
 from rabbitpy.simple import delete_exchange
-
-import logging
-
-try:
-    from logging import NullHandler
-except ImportError:
-    # Python 2.6 does not have a NullHandler
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
-
-logging.getLogger('rabbitpy').addHandler(NullHandler())
-
-DEBUG = False
