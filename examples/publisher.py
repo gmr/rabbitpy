ee7973b7049dec296cb52efaaab05613ae519da9
dylan d <eris.has.a.dad@gmail.com>
2016-02-27 09:46:57 -0800

    Examples fixed for version 0.22.0+
    - Since opinionated is no longer the default, I manually add
      the UUID/timestamp.

--- examples/publisher.py
+++ examples/publisher.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import rabbitpy
 import logging
+import datetime
+import uuid
 logging.basicConfig(level=logging.DEBUG)
 
 # Use a new connection as a context manager
@@ -26,7 +28,9 @@ with rabbitpy.Connection('amqp://guest:guest@localhost:5672/%2f') as conn:
                                    'adipiscing elit.',
                                    {'content_type': 'text/plain',
                                     'delivery_mode': 1,
-                                    'message_type': 'Lorem ipsum'})
+                                    'message_type': 'Lorem ipsum',
+                                    'timestamp': datetime.datetime.now(),
+                                    'message_id': uuid.uuid1()})
 
         # Publish the message
         message.publish(exchange, 'test-routing-key')
