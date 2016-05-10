fd0397f6dd2732954cea221c7fcb04e7c36c407b
Gavin M. Roy <gavinr@aweber.com>
2016-05-10 16:30:05 -0400

    Bump the version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'Topic :: Software Development :: Libraries']
 
 setuptools.setup(name='rabbitpy',
-                 version='0.26.2',
+                 version='0.27.0',
                  description=desc,
                  long_description=open('README.rst').read(),
                  author='Gavin M. Roy',
