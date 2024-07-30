e82bd26fde1f09f5ba316e19221b9d30beed046a
Gavin M. Roy <gavinr@aweber.com>
2024-07-30 17:05:04 -0400

    Drop Python3, Add Type Annotations, Refactor a bit

--- rabbitpy/utils.py
+++ rabbitpy/utils.py
@@ -1,103 +1,71 @@
-"""Utilities to make Python 3 support easier, providing wrapper methods which
-can call the appropriate method for either Python 2 or Python 3 but creating
-a single API point for rabbitpy to use.
+"""
+Utilities
+=========
 
 """
 import collections
-# pylint: disable=unused-import,import-error
-try:
-    import Queue as queue
-except ImportError:
-    import queue
+import logging
 import platform
 import socket
-# pylint: disable=import-error
-try:
-    from urllib import parse as _urlparse
-except ImportError:
-    import urlparse as _urlparse
+import typing
+from urllib import parse
 
-from pamqp import PYTHON3
-
+LOGGER = logging.getLogger('rabbitpy')
 PYPY = platform.python_implementation() == 'PyPy'
 
-Parsed = collections.namedtuple('Parsed',
-                                'scheme,netloc,path,params,query,fragment,'
-                                'username,password,hostname,port')
+Parsed = collections.namedtuple(
+    'Parsed',
+    'scheme,netloc,path,params,query,fragment,username,password,hostname,port')
 
 
-def maybe_utf8_encode(value):
+def maybe_utf8_encode(value: typing.Union[str, bytes]) -> bytes:
     """Cross-python version method that will attempt to utf-8 encode a string.
 
-    :param mixed value: The value to maybe encode
-    :return: str
+    :param value: The value to maybe encode
 
     """
-
-    if PYTHON3:
-        if is_string(value) and not isinstance(value, bytes):
-            return bytes(value, 'utf-8')
-        return value
-    if isinstance(value, unicode):  # pylint: disable=undefined-variable
+    try:
         return value.encode('utf-8')
-    return value
+    except AttributeError:
+        return value
 
 
-def parse_qs(query_string):
-    """Cross-python version method for parsing a query string.
-
-    :param str query_string: The query string to parse
-    :return: tuple
-    """
-    return _urlparse.parse_qs(query_string)
-
-
-def urlparse(url):
+def urlparse(url: str) -> Parsed:
     """Parse a URL, returning a named tuple result.
 
-    :param str url: The URL to parse
-    :rtype: collections.namedtuple
+    :param url: The URL to parse
 
     """
     value = 'http%s' % url[4:] if url[:4] == 'amqp' else url
-    parsed = _urlparse.urlparse(value)
+    parsed = parse.urlparse(value)
     return Parsed(parsed.scheme.replace('http', 'amqp'), parsed.netloc,
                   parsed.path, parsed.params, parsed.query, parsed.fragment,
-                  parsed.username,
-                  parsed.password,
-                  parsed.hostname, parsed.port)
+                  parsed.username, parsed.password, parsed.hostname,
+                  parsed.port)
 
 
-def unquote(value):
-    """Cross-python version method for unquoting a URI value.
-
-    :param str value: The value to unquote
-    :rtype: str
-
-    """
-    return _urlparse.unquote(value)
-
-
-def is_string(value):
-    """Check to see if the value is a string in Python 2 and 3.
-
-    :param bytes|str|unicode value: The value to check
-    :rtype: bool
-
-    """
-    checks = [isinstance(value, bytes), isinstance(value, str)]
-    if not PYTHON3:
-        # pylint: disable=undefined-variable
-        checks.append(isinstance(value, unicode))
-    return any(checks)
-
-
-def trigger_write(sock):
-    """Notifies the IO loop we need to write a frame by writing a byte
-    to a local socket.
-
-    """
+def trigger_write(sock: socket.socket) -> None:
     try:
         sock.send(b'0')
     except socket.error:
         pass
+
+
+class DebuggingOptimizationMixin:
+    """Micro-optimization to avoid logging overhead"""
+
+    def __init__(self):
+        self._debugging: typing.Optional[bool] = None
+
+    @property
+    def _is_debugging(self) -> bool:
+        """Indicates that something has set the logger to ``logging.DEBUG``
+        to perform a minor micro-optimization preventing ``LOGGER.debug`` calls
+        when they are not required.
+
+        :return: bool
+
+        """
+        if self._debugging is None:
+            self._debugging = LOGGER.getEffectiveLevel() == logging.DEBUG
+        return self._debugging
