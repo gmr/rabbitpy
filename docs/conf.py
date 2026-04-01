0ebc1cc62d8a580156b4880359ab867a2fbde667
Gavin M. Roy <gavinr@aweber.com>
2026-03-31 20:36:12 -0400

    Apply ruff formatting to docs/conf.py
    Co-Authored-By: Gavin M. Roy <gavinmroy@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -6,25 +6,29 @@ import sys
 
 sys.path.insert(0, '../')
 
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.doctest',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.autosummary',
-              'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = 'rabbitpy'
 copyright = '2013 - 2025, Gavin M. Roy'
 
-import rabbitpy  # noqa: E402 I001
+import rabbitpy  # noqa: E402
+
 release = rabbitpy.__version__
 version = '.'.join(release.split('.')[0:1])
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
-intersphinx_mapping = {'pamqp': ('https://pamqp.readthedocs.org/en/latest/',
-                                 None),
-                       'python': ('https://docs.python.org/3/', None)}
+intersphinx_mapping = {
+    'pamqp': ('https://pamqp.readthedocs.org/en/latest/', None),
+    'python': ('https://docs.python.org/3/', None),
+}
 
 html_theme = 'default'
 html_static_path = ['_static']
